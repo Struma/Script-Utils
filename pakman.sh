a8ebaf424fd634e926ce25e9ce3148c6b528f6bc
Julio Domínguez <43100376+Struma@users.noreply.github.com>
2018-09-18 23:25:01 -0400

    Update pakman.sh

--- pakman.sh
+++ pakman.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
+#This is an mkchromecast script that cast the david Pakman Show to your tv.
+#He's ok, run everyday at 4:15pm EST
+#By Struma - github.com/Struma 2018
+#Url Template: http://traffic.libsyn.com/davidpakman/davidpakmanshow-2018-08-24.mp3 
 
-#this is a pakman script that plays his shit from a script
-# http://traffic.libsyn.com/davidpakman/davidpakmanshow-2018-08-24.mp3 
 dater=$( date +%Y-%m-%d  )
 pakman_url="http://traffic.libsyn.com/davidpakman/davidpakmanshow-"
 endr=".mp3"
 final_url="$pakman_url$dater$endr"
+#mkchromecast is great, throw in the --control option for vol control
 mkchromecast --source-url $final_url --control 
-#echo $final_url
+
